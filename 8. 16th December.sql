fc7ae241ce63e5396780664fd7b781e85ca60b00
Lakshay Gautam <lakshaygautam7309@gmail.com>
2025-12-22 08:37:31 +0530

    Upto 53% complete

--- 8. 16th December.sql
+++ 8. 16th December.sql
@@ -47,8 +47,50 @@ select continent, sum(population) from country group by continent;
   -- "where sum(population) > 100" [❌ Wrong, sum(population) does not 
 							     -- exist in our table, It was the output. 
                                  -- so we cannot apply "sum()" in where]
-	
-select continent, sum(population) from country where sum(population)>100 group by continent;
 
+# RULE 📝: Where will only filter those data which exist in my table.
+
+
+# Interview Question
+# Ques: What is the difference between where and having clause? (Very common question for Interview)
+/* The WHERE clause is used to filter individual rows from a table before 
+   grouping. In contrast, the HAVING clause is used to filter results based 
+   on aggregate columns (such as COUNT, SUM, MAX, or MIN) after the grouping 
+   has been performed
+    
+   The WHERE clause is applied before GROUP BY, while the HAVING clause is applied 
+   after GROUP BY.
+*/
+
+-- having to filter data based on aggregate column like sum, count, max
+select continent, sum(population) from country group by continent 
+having sum(population)>100;
+
+
+# Question: You have to find out the continent name, total population 
+-- for each continent where the country is having a life expectancy 
+-- of greater than 35.0 % 
+
+select continent, sum(population) from country where LifeExpectancy > 35.00
+group by continent; # Read Note 👇 
+# Note: We didn't use HAVING here because there is no aggregate function 
+-- on LifeExpectancy. We only use having to filter aggregate column.
+-- LifeExpectancy is already present so we use where, here.  
+
+
+# Question-1: You need to find out the government form, total country 
+-- for each government form where the total number of country should 
+-- be greater than 30
+select GovernmentForm, count(name) from country group by GovernmentForm 
+having count(name) > 30.00;
+# Or You Can Also Do Like This, Both are Same. 
+select GovernmentForm, count(name) as totalcountry from country 
+group by GovernmentForm having totalcountry > 30.00;
+
+# Question-2: You need to find out GovernmentForm, the total country
+-- for each GovernmentForm only for the country having their capital 
+-- greater than 30 and total population greater than 3 lakh. 
+
+select * from country;
 
 
