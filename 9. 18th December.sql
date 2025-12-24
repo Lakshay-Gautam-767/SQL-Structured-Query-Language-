98113665f2a87446fa469fcba751bdad4a0050f1
Lakshay Gautam <lakshaygautam7309@gmail.com>
2025-12-24 08:36:42 +0530

    Upto 80% completed

--- 9. 18th December.sql
+++ 9. 18th December.sql
@@ -27,7 +27,9 @@
 -- 4. KEY TERMS IN RELATIONSHIPS
 -- ============================
 -- > PRIMARY KEY : Makes a column UNIQUE for every row (No duplicates, No NULLs).
+				-- So that we can make (One to One, One to Many, Many to One) relationship.                   
 -- > FOREIGN KEY : A column that points to the Primary Key of another table.
+				-- Foreign Key is used to set the relationship with another table.  
 
 -- 5. TYPES OF RELATIONSHIPS
 -- =========================
@@ -45,4 +47,79 @@
 
 use world;
 select * from city; 
+select * from country;
+
+-- 7. IDENTIFYING THE PRIMARY KEY
+-- ==============================
+-- Problem: Columns like 'Name', 'District' or 'Population' can have 
+-- duplicates (e.g., Jaipur).
+-- Solution: Need a unique identifier for every record.
+
+-- * How to Verify: 
+-- i) First Method: If inbuild database has mentioned column as primary key
+-- Rule: Use [DESCRIBE table_name;] to check the Primary Key.
+describe city; 	# ID is Primary Key here. 
+describe country; # Code is Primary Key here. 
+
+-- ii) Second Method: 
+select count(code), count(distinct(code)) from country; # Both are same, 
+-- that means no duplicate values are present so we can choose it as a 
+-- primary key. 
+
+-- iii) Third Method: Use common sense that if value are duplicate then it
+-- can't be the primary key. 
+
+-- * Observations:
+--   - City Table    : 'ID' is the PK (Unique & No NULLs).
+--   - Country Table : 'Code' (e.g., IND, AFG) is the PK.
+
+
+-- 8. ESTABLISHING RELATIONSHIPS (FOREIGN KEY)
+-- ==========================================
+-- Definition: A Foreign Key in a "Child Table" refers to the Primary Key 
+--             in a "Parent Table".
+
+-- * Logic in World Database:
+--   - Parent (Country) : PK(Primary Key) is 'Code'.
+		-- Because 'Code' is unique in each row of country table (like 'IND' only one time)
+--   - Child (City)    : FK(Foreigner Key) is 'CountryCode'.
+		-- Because  'CountryCode' have duplicate values. (like 'AFG' many times)
+-- * Why? 'CountryCode' can repeat in City table (One country has many cities),
+--   linking back to one unique country.
+
+
+-- 9. SQL JOINS: CONNECTING TABLES
+-- ===============================
+/* If we want to connect 2 or more tables than we use joins. Join helps to 
+   connect the table with the help of Foreign Key and Primary Key. We use 
+   join so that we can find the information from another table. */
+-- Use JOIN to combine rows based on related columns.
+
+-- [BEST PRACTICE: ALIASES]
+-- To avoid "Ambiguity" (jab do tables mein same column name ho, jaise 'Name'),
+-- we use short names.
+-- Example: FROM City AS c | FROM Country AS co
+-- Access as: c.Name (City) vs co.Name (Country)
+
+select * from city;
+select c.name, c.countrycode from city as c;
+
+-- Example A: Joining City and Country (To get Continent)
+SELECT cy.id, cy.name, cy.countrycode, cnt.code, 
+cnt.name, cnt.continent FROM city AS cy
+JOIN country AS cnt 
+where cy.countrycode = cnt.code;    # (where) This is old way
+
+# BOTH ARE SAME
+
+SELECT cy.id, cy.name, cy.countrycode, cnt.code, 
+cnt.name, cnt.continent FROM country AS cnt
+JOIN city AS cy
+ON cy.countrycode = cnt.code;       # (on) This is a modern and standard way. 
+
+-- ===========================================================================
+# Question: Find the city name, city population, the country name along
+-- with the governmentForm for each city.
+select cy.name, cy.population, cnt.name, cnt.governmentform from city as cy
+join country as cnt on cy.countrycode = cnt.code;
 -- *************************** END OF NOTES ***************************
